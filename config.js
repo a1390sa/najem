4fa3c511467eb97f2f8d0bf63cdaf95f2c5bbc20
a1390sa <a1390sa@users.noreply.github.com>
2026-07-30 09:12:24 +0300

    Fix Supabase JWT anonKey configuration

--- config.js
+++ config.js
@@ -4,5 +4,5 @@
  */
 window.SUPABASE_CONFIG = {
     url: "https://mlokzejdaflnsvrvzruz.supabase.co",       // رابط مشروعك السحابي في Supabase
-    anonKey: "sb_publishable_1aj-HZpe-bhi51Yc698RlQ_zLY4I_Q-"    // مفتاح Anon Key الخاص بمشروعك
+    anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1sb2t6ZWpkYWZsbnN2cnZ6cnV6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM5MDY2OTIsImV4cCI6MjA5OTQ4MjY5Mn0.t_Hqos7JSAPbQzeRBn3hYFn7rgSikXp-KWhxbFugRk0"    // مفتاح Anon Key الخاص بمشروعك
 };
